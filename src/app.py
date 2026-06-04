863cda0ad50dc1019a30db651e841e3704a951db
Bryan Lima <bryanlimaz630@gmail.com>
2026-06-03 23:59:03 -0300

    feat: add endpoint to retrieve posts from the feed

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ from src.schemas import PostCreate, PostResponse
 from src.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+from sqlalchemy import select
 
 
 @asynccontextmanager
@@ -27,3 +28,23 @@ async def upload_file(
     await session.commit()
     await session.refresh(post)
     return post
+
+@app.get("/feed")
+async def get_feed(session: AsyncSession = Depends(get_async_session)):
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()]
+
+    posts_data = []
+    for post in posts:
+        posts_data.append(
+            {
+                "id": str(post.id),
+                "caption": post.caption,
+                "url": post.url,
+                "file_type": post.file_type,
+                "file_name": post.file_name,
+                "created_at": post.created_at.isoformat(),
+            }
+        )
+
+    return {"posts": posts_data}
